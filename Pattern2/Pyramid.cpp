6b92ee40df1655e999b4d637717eb009e381751c
Sahil-husen <sahilhussainsahil76@gmail.com>
2025-09-16 23:30:15 +0530

    Pyramid Pattern added

---
+++ Pattern2/Pyramid.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+
+
+    int row =4;
+
+    for(int i=1;i<=row;i++){
+        for(int space = 1;space<=row-i;space++){
+            cout<<" ";
+        }
+
+        for(int j =1;j<=i;j++){
+            cout<<"*";
+        }
+         
+
+        cout<<endl;
+    }
+}
