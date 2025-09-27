07bc558355c334daa6a3e4a13cc8a1abe1cee50a
Sahil-husen <sahilhussainsahil76@gmail.com>
2025-09-28 03:43:25 +0530

    DeleteCharAtGivenPosition

--- String/insertGivenString.cpp
+++ String/insertGivenString.cpp
@@ -40,12 +40,46 @@ string insertSubString(string main, string sub, int pos)
 }
 
 
+void resizes(string &str,int le){
+    
+      string res = "";
+    for(int i =0;i<le;i++){
+      
+        res+=str[i];
+    }
+    str = res;
+
+}
+
+string deleteAtPosition(string &str,int nth,int pos ){
+
+     
+
+    int length = len(str);
+
+    // for(int i= pos;i<length-nth;i++){
+    //     str[i] = str[i+nth];
+    // }
+    
+    //  resizes(str,(length-nth));
+    // return str;
+
+    for(int i=pos;i<length;i++){
+        str[i] = str[i+nth];
+    }
+    str.resize(length-nth);
+
+    return str;
+
+}
+
 
 
 int main()
 {
-    string main = "helloworld";
+    string main = "computer";
     string sub = "abc";
     int pos = 5;
-    cout << insertSubString(main, sub, pos);
+    // cout << insertSubString(main, sub, pos);
+    cout<<deleteAtPosition(main,4,2);
 }
--- String/tempCodeRunnerFile.cpp
+++ String/tempCodeRunnerFile.cpp
@@ -1,3 +1,17 @@
-for(int i = mainStr.length();i<=13;i--){
-        //     mainStr[i] = mainStr[mainLen];
-        // }
+
+    return main;
+}
+
+
+string deleteAtPosition(string &str,int nth,int pos ){
+
+    int length = len(str);
+
+    for(int i=pos-1;i<=nth;i++){
+        if(str[i]!=str[nth++]){
+            return str;
+        }
+        else{
+            continue;
+        }
+    }  
